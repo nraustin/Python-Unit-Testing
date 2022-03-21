d2ce2ead97cb2af43268d97e68c503fb2c8d5ff3
Phil <philroberts13@aol.com>
2022-03-21 17:12:13 -0600

    first test

---
+++ test/test_roman_numerals_unittest.py
@@ -0,0 +1,8 @@
+import unittest
+from app.roman_numerals import parse
+
+class TestRomanNumerals(unittest.TestCase):
+    def test_i(self):
+        value = parse("I")
+
+        self.assertEqual(value, 1)
